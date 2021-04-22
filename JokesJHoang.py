143a950a3b0108c908d3801c8a4fb90321013054
Joha2001 <57204571+Joha2001@users.noreply.github.com>
2021-04-22 01:03:22 -0400

    Jokes Added

---
+++ JokesJHoang.py
@@ -0,0 +1,18 @@
+# This program was created by John Hoang on July 8, 2018
+# This runs a few jokes!
+print('What do you get when you cross a snowman with a vampire?')
+input()
+print('Frostbite!')
+print()
+print('What do dentists call an astronaut\'s cavity?')
+input()
+print('A black hole!')
+print()
+print('Knock knock.')
+input()
+print("Who's there?")
+input()
+print('Interrupting cow.')
+input()
+print('Interrupting cow wh', end='')
+print('-MOO!')
